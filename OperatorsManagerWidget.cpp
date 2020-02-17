df4f98b8c70f14f912af893cbe1e988d00901e05
Aleksandr Khryshchuk <khrishuk@gmail.com>
2020-02-17 22:25:49 +0200

    - Floating button for operators adding; - Icons resource added; - Initial doxygen file added.

--- OperatorsManagerWidget.cpp
+++ OperatorsManagerWidget.cpp
@@ -1,11 +1,14 @@
+#include <QPushButton>
+#include <QResizeEvent>
+
 #include "OperatorsManagerWidget.hpp"
 #include "ui_OperatorsManagerWidget.h"
 
 OperatorsManagerWidget::OperatorsManagerWidget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::OperatorsManagerWidget)
+    : QWidget(parent), ui(new Ui::OperatorsManagerWidget), pAddButtonPtr(nullptr)
 {
     ui->setupUi(this);
+    createFloatingAddOperatorButton();
 }
 
 OperatorsManagerWidget::~OperatorsManagerWidget()
@@ -13,3 +16,33 @@ OperatorsManagerWidget::~OperatorsManagerWidget()
     delete ui;
 }
 
+void OperatorsManagerWidget::onAddOperatorButtonClicked()
+{
+
+}
+
+void OperatorsManagerWidget::createFloatingAddOperatorButton()
+{
+    const QSize buttonSize(36, 36);
+    const QString iconPath(":/ui/icons/plus.svg");
+
+    pAddButtonPtr = new QPushButton(this);
+    pAddButtonPtr->setFlat(true);
+    pAddButtonPtr->setIcon(QIcon(iconPath));
+    pAddButtonPtr->setIconSize(buttonSize);
+    pAddButtonPtr->resize(buttonSize);
+
+    connect(pAddButtonPtr, &QPushButton::clicked,
+            this, &OperatorsManagerWidget::onAddOperatorButtonClicked);
+}
+
+void OperatorsManagerWidget::resizeEvent(QResizeEvent *event)
+{
+    const int buttonWidgetSizeOffset = 24;
+    const QSize windowSize = event->size();
+
+    pAddButtonPtr->move(windowSize.width() - (pAddButtonPtr->width() + buttonWidgetSizeOffset),
+                        windowSize.height() - (pAddButtonPtr->height() + buttonWidgetSizeOffset));
+
+    QWidget::resizeEvent(event);
+}
--- OperatorsManagerWidget.hpp
+++ OperatorsManagerWidget.hpp
@@ -6,14 +6,35 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class OperatorsManagerWidget; }
 QT_END_NAMESPACE
 
+class QPushButton;
+
 class OperatorsManagerWidget : public QWidget
 {
     Q_OBJECT
 
 public:
+    /*!
+     * \param[in] parent Parent widget pointer.
+     *
+     * Includes creating of floating button for adding operators.
+     */
     OperatorsManagerWidget(QWidget *parent = nullptr);
     ~OperatorsManagerWidget();
 
+private slots:
+    /// Creates operator adding dialog.
+    void onAddOperatorButtonClicked();
+
+private:
+    /// Initializes floating button for operators adding.
+    void createFloatingAddOperatorButton();
+
+protected:
+    /// Resize event override includes floating button movement.
+    void resizeEvent(QResizeEvent *event) Q_DECL_OVERRIDE;
+
 private:
     Ui::OperatorsManagerWidget *ui;
+    QPushButton *pAddButtonPtr;
+
 };
